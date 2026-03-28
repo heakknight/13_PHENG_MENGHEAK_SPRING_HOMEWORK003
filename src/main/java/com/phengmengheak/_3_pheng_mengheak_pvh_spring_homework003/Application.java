9d260612854b460fd1b6fa017cc61bc0bc777dad
heakknight <heakmeng42@gmail.com>
2026-03-28 23:00:40 +0700

    Created folder project

---
+++ src/main/java/com/phengmengheak/_3_pheng_mengheak_pvh_spring_homework003/Application.java
@@ -0,0 +1,13 @@
+package com.phengmengheak._3_pheng_mengheak_pvh_spring_homework003;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
